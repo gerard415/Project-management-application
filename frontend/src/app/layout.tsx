0da97e280509d5b326a13fe5688ef05eb941ecd5
gerard415 <chinedumike85@gmail.com>
2024-11-16 17:42:03 -0500

    deploying to github

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import DashboardWrapper from "./dashboardWrapper";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <DashboardWrapper>
+          {children}
+        </DashboardWrapper>
+      </body>
+    </html>
+  );
+}
